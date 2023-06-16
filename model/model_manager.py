60ab1fc5c95bc22b14195c4cc27228549fed175d
paul910 <paul.ehrhard@web.de>
2023-06-16 10:23:43 +0200

    test

--- model/model_manager.py
+++ model/model_manager.py
@@ -15,7 +15,7 @@ class ModelManager:
     def __init__(self, config: Config, diffusion_utils: DiffusionUtils):
         self.config = config
         self.diffusion_utils = diffusion_utils
-        self.model = Unet(self.config.MODEL_DEPTH, self.config.MODEL_START_CHANNELS, self.config.TIME_EMB_DIM)
+        self.model = Unet(self.config.MODEL_DEPTH, self.config.MODEL_START_CHANNELS, self.config.TIME_EMB_DIM).to(self.config.DEVICE)
         self.optimizer = Adam(self.model.parameters(), lr=self.config.LEARNING_RATE)
         self.data_loader = get_adj_dataloader(self.config.DATA_PATH, self.config.BATCH_SIZE, self.config.MODEL_DEPTH)
 
