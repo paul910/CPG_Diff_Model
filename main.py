0e74e67b20d0eec427d94c8d1c3e7070731587b2
paul910 <paul.ehrhard@web.de>
2023-06-19 11:11:58 +0200

    fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     diffusion_utils = DiffusionUtils(config)
     dataloader = get_adj_dataloader(config.DATA_PATH, config.BATCH_SIZE, config.MODEL_DEPTH)
     model_manager = ModelManager(config, diffusion_utils, dataloader)
-    #model_manager.train_model()
+    #model_manager.sample_plot_image(64)
     model_manager.generate_graphs(1000)
 
 
