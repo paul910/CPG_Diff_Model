b976baf97d3d5a03dc4f43551771560aae2f53e4
paul910 <paul.ehrhard@web.de>
2023-06-19 10:57:50 +0200

    fix

--- model/model_manager.py
+++ model/model_manager.py
@@ -107,6 +107,7 @@ class ModelManager:
                 t = torch.full((1,), i, device=self.config.DEVICE, dtype=torch.long)
                 adj = self.diffusion_utils.sample_timestep(adj, t, self.model)
                 adj = torch.clamp(adj, -1.0, 1.0)
+                adj = adj.squeeze().squeeze().numpy()
                 utils.normalize(adj)
 
             torch.save(adj, f'output/graphs/graph_{num}.pt')
